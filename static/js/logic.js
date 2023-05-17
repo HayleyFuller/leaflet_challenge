b7b7df043c6a3ff24ec7d9c3373918669e714e0c
HayleyFuller <122294137+HayleyFuller@users.noreply.github.com>
2023-05-17 11:27:51 +0100

    All files

---
+++ static/js/logic.js
@@ -0,0 +1,81 @@
+// create the map
+let earthMap = L.map("map", {
+    center: [36.00, -110.71],
+    zoom: 5
+});
+
+// add the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(earthMap);
+
+// store the API endpoint as a query Url. There are several links (for data of the past 7 days)listed.
+//Any can be used by removing the /// at the begining of the line.
+
+// All earthquakes
+let urlQuery = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+//Link for M1.0+ Earthquakes
+//let urlQuery = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"
+
+// Link for M2.5+ Earthquakes
+/// let urlQuery = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonn"
+
+//Link for M4.5+ Earthquakes
+/// let urlQuery = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+
+//Link for significate Earthquakes
+/// urlQuery = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+
+// fetch the data with D3.
+d3.json(urlQuery).then(function(data) {
+
+    // create the markers
+    function createMarker(feature, y) {
+        return L.circleMarker(y, {
+            opacity: 2,
+            fillOpacity: 1,
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            radius: feature.properties.mag * 5,
+            color: "white",
+            weight: 0.5
+        });
+    }
+
+    // assign the markers a color
+    function markerColor(y) {
+        return y > 90 ? '#d73027' :
+            y > 70 ? '#fc8d59' :
+            y > 50 ? '#fee08b' :
+            y > 30 ? '#d9ef8b' :
+            y > 10 ? '#91cf60' :
+                     '#1a9850' ;   
+    };
+
+    //add the data to the map
+    L.geoJson(data, {
+        pointToLayer: createMarker,
+        // add the bind pop up
+       onEachFeature: function (feature, layer) {
+           layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+        }
+    }).addTo(earthMap);
+
+    //add a legend
+    let legend = L.control({position: 'bottomleft'});
+
+    legend.onAdd = function () {
+        let div = L.DomUtil.create('div', 'info legend'),
+            y = [-10, 10, 30, 50, 70, 90];
+
+        for (let i = 0; i < y.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + markerColor(y[i] + 1) + '"></i> ' +
+                y[i] + (y[i + 1] ? '&ndash;' + y[i + 1] + '<br>' : '+');
+        }    
+        return div;
+    };
+
+    // add the legend to the map
+    legend.addTo(earthMap);
+});
